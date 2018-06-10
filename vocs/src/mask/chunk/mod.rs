1de31c257fc8a225aa5a570258cc222ddf096417
coderbot16 <coderbot16@gmail.com>
2018-06-10 12:05:34 -0700

    Add an "inhabited" field to ChunkMask for easy detection of whether certain blocks have bits set

--- vocs/src/mask/chunk/mod.rs
+++ vocs/src/mask/chunk/mod.rs
@@ -11,12 +11,18 @@ pub use self::scan::*;
 const FALSE_REF: &bool = &false;
 const TRUE_REF:  &bool = &true;
 
-pub struct ChunkMask(Box<[u64; 64]>);
+pub struct ChunkMask {
+	blocks: Box<[u64; 64]>,
+	inhabited: u64
+}
+
 impl ChunkMask {
 	pub fn combine(&mut self, other: &ChunkMask) {
-		for (target, other) in self.blocks_mut().iter_mut().zip(other.blocks().iter()) {
+		for (target, other) in self.blocks.iter_mut().zip(other.blocks.iter()) {
 			*target = *target | *other;
 		}
+
+		self.inhabited |= other.inhabited;
 	}
 
 	pub fn set_neighbors(&mut self, position: ChunkPosition) {
@@ -29,11 +35,7 @@ impl ChunkMask {
 	}
 
 	pub fn blocks(&self) -> &[u64; 64] {
-		&self.0
-	}
-
-	pub fn blocks_mut(&mut self) -> &mut [u64; 64] {
-		&mut self.0
+		&self.blocks
 	}
 }
 
@@ -47,53 +49,64 @@ impl ChunkStorage<bool> for ChunkMask {
 	}
 
 	fn fill(&mut self, value: bool) {
-		if value {
-			for value in self.0.iter_mut() {
-				*value = u64::max_value();
-			}
-		} else {
-			for value in self.0.iter_mut() {
-				*value = 0;
-			}
+		let fill = if value { u64::max_value() } else { 0 };
+
+		for value in self.blocks.iter_mut() {
+			*value = fill;
 		}
+
+		self.inhabited = fill;
 	}
 }
 
 impl Mask<ChunkPosition> for ChunkMask {
 	fn set_true(&mut self, position: ChunkPosition) {
 		let index = position.yzx() as usize;
+		let (block_index, sub_index) = (index / 64, index % 64);
 
-		self.0[index / 64] |= 1 << (index % 64);
+		self.inhabited |= 1 << block_index;
+		self.blocks[block_index] |= 1 << sub_index;
 	}
 
 	fn set_false(&mut self, position: ChunkPosition) {
 		let index = position.yzx() as usize;
+		let (block_index, sub_index) = (index / 64, index % 64);
 
-		self.0[index / 64] &= !(1 << (index % 64));
+		let cleared = self.blocks[block_index] & !(1 << sub_index);
+		self.blocks[block_index] = cleared;
+
+		let cleared_inhabited = self.inhabited & !(1 << block_index);
+		self.inhabited = cleared_inhabited | (((cleared != 0) as u64) << block_index);
 	}
 
 	fn set_or(&mut self, position: ChunkPosition, value: bool) {
 		let index = position.yzx() as usize;
+		let (block_index, sub_index) = (index / 64, index % 64);
 
-		self.0[index / 64] |= (value as u64) << (index % 64);
+		self.inhabited |= (value as u64) << block_index;
+		self.blocks[block_index] |= (value as u64) << sub_index;
 	}
 
 	fn set(&mut self, position: ChunkPosition, value: bool) {
 		let index = position.yzx() as usize;
+		let (block_index, sub_index) = (index / 64, index % 64);
 
-		let array_index = index / 64;
-		let shift = index % 64;
+		let cleared = self.blocks[block_index] & !(1 << sub_index);
+		let block = cleared | ((value as u64) << sub_index);
 
-		let cleared = self.0[array_index] & !(1 << shift);
-		self.0[array_index] = cleared | ((value as u64) << shift)
+		// Update inhabited, using a similar method to updating the bitfield.
+		let cleared_inhabited = self.inhabited & !(1 << block_index);
+		self.inhabited = cleared_inhabited | (((block != 0) as u64) << block_index);
+
+		self.blocks[block_index] = block;
 	}
 
 	fn count_ones(&self) -> u32 {
-		self.0.iter().fold(0, |state, value| state + value.count_ones())
+		self.blocks.iter().fold(0, |state, value| state + value.count_ones())
 	}
 
 	fn count_zeros(&self) -> u32 {
-		self.0.iter().fold(0, |state, value| state + value.count_zeros())
+		self.blocks.iter().fold(0, |state, value| state + value.count_zeros())
 	}
 }
 
@@ -103,26 +116,32 @@ impl Index<ChunkPosition> for ChunkMask {
 	fn index(&self, position: ChunkPosition) -> &bool {
 		let index = position.yzx() as usize;
 
-		if (self.0[index / 64] >> (index % 64))&1 == 1 { TRUE_REF } else { FALSE_REF }
+		if (self.blocks[index / 64] >> (index % 64))&1 == 1 { TRUE_REF } else { FALSE_REF }
 	}
 }
 
 impl Clone for ChunkMask {
 	fn clone(&self) -> Self {
-		ChunkMask(Box::new([
-			self.0[ 0], self.0[ 1], self.0[ 2], self.0[ 3], self.0[ 4], self.0[ 5], self.0[ 6], self.0[ 7], self.0[ 8], self.0[ 9],
-			self.0[10], self.0[11], self.0[12], self.0[13], self.0[14], self.0[15], self.0[16], self.0[17], self.0[18], self.0[19],
-			self.0[20], self.0[21], self.0[22], self.0[23], self.0[24], self.0[25], self.0[26], self.0[27], self.0[28], self.0[29],
-			self.0[30], self.0[31], self.0[32], self.0[33], self.0[34], self.0[35], self.0[36], self.0[37], self.0[38], self.0[39],
-			self.0[40], self.0[41], self.0[42], self.0[43], self.0[44], self.0[45], self.0[46], self.0[47], self.0[48], self.0[49],
-			self.0[50], self.0[51], self.0[52], self.0[53], self.0[54], self.0[55], self.0[56], self.0[57], self.0[58], self.0[59],
-			self.0[60], self.0[61], self.0[62], self.0[63]
-		]))
+		ChunkMask {
+			blocks: Box::new([
+				self.blocks[ 0], self.blocks[ 1], self.blocks[ 2], self.blocks[ 3], self.blocks[ 4], self.blocks[ 5], self.blocks[ 6], self.blocks[ 7], self.blocks[ 8], self.blocks[ 9],
+				self.blocks[10], self.blocks[11], self.blocks[12], self.blocks[13], self.blocks[14], self.blocks[15], self.blocks[16], self.blocks[17], self.blocks[18], self.blocks[19],
+				self.blocks[20], self.blocks[21], self.blocks[22], self.blocks[23], self.blocks[24], self.blocks[25], self.blocks[26], self.blocks[27], self.blocks[28], self.blocks[29],
+				self.blocks[30], self.blocks[31], self.blocks[32], self.blocks[33], self.blocks[34], self.blocks[35], self.blocks[36], self.blocks[37], self.blocks[38], self.blocks[39],
+				self.blocks[40], self.blocks[41], self.blocks[42], self.blocks[43], self.blocks[44], self.blocks[45], self.blocks[46], self.blocks[47], self.blocks[48], self.blocks[49],
+				self.blocks[50], self.blocks[51], self.blocks[52], self.blocks[53], self.blocks[54], self.blocks[55], self.blocks[56], self.blocks[57], self.blocks[58], self.blocks[59],
+				self.blocks[60], self.blocks[61], self.blocks[62], self.blocks[63]
+			]),
+			inhabited: self.inhabited
+		}
 	}
 }
 
 impl Default for ChunkMask {
 	fn default() -> Self {
-		ChunkMask(Box::new([0; 64]))
+		ChunkMask {
+			blocks: Box::new([0; 64]),
+			inhabited: 0
+		}
 	}
 }
