7b19b9ba57bb3b0cf4f0f00c6e5d8017ecb7467b
coderbot <coderbot16@gmail.com>
2019-04-13 12:50:19 -0700

    Commit partially complete work on chunk-level shape generation

--- i73/i73-terrain/src/overworld/shape.rs
+++ i73/i73-terrain/src/overworld/shape.rs
@@ -4,10 +4,11 @@ use i73_biome::climate::ClimateSource;
 use i73_shape::height::HeightSource;
 use i73_shape::volume::{TriNoiseSource, ShapeSettings, trilinear128};
 use i73_base::Pass;
-use vocs::position::{ColumnPosition, GlobalColumnPosition};
+use vocs::position::{ColumnPosition, GlobalColumnPosition, ChunkPosition};
 use vocs::view::ColumnMut;
-use i73_base::Block;
+use i73_base::{Block, math};
 use i73_shape::height::lerp_to_layer;
+use std::convert::TryInto;
 
 pub struct ShapeBlocks {
 	pub solid: Block,
@@ -45,7 +46,7 @@ impl Pass for ShapePass {
 
 		let climate_chunk = self.climate.chunk(block_offset);
 
-		let mut field = [[[0f64; 5]; 17]; 5];
+		let mut field = [[[0f64; 5]; 5]; 17];
 
 		for x in 0..5 {
 			for z in 0..5 {
@@ -57,29 +58,77 @@ impl Pass for ShapePass {
 				for y in 0..17 {
 					let tri = self.tri.sample(Vector3::new(offset.x + x as f64, y as f64, offset.y + z as f64), y);
 
-					field[x][y][z] = self.field.compute_noise_value(y as f64, height, tri);
+					field[y][z][x] = self.field.compute_noise_value(y as f64, height, tri);
 				}
 			}
 		}
 
-		target.ensure_available(self.blocks.air.clone());
-		target.ensure_available(self.blocks.solid.clone());
+		for (index, chunk) in target.0.iter_mut().enumerate().take(8) {
+			chunk.ensure_available(self.blocks.air.clone());
+			chunk.ensure_available(self.blocks.solid.clone());
 
-		let (mut blocks, palette) = target.freeze_palette();
+			let (mut blocks, palette) = chunk.freeze_palette();
 
-		let air   = palette.reverse_lookup(&self.blocks.air).unwrap();
-		let solid = palette.reverse_lookup(&self.blocks.solid).unwrap();
+			let air   = palette.reverse_lookup(&self.blocks.air).unwrap();
+			let solid = palette.reverse_lookup(&self.blocks.solid).unwrap();
 
-		for i in 0..32768 {
-			let position = ColumnPosition::from_yzx(i);
+			let new_field: &[[[f64; 5]; 5]; 3] = (&field[1..3]).try_into().unwrap();
 
-			let block = if trilinear128(&field, position) > 0.0 {
-				&solid
-			} else {
-				&air
-			};
+			for position in ChunkPosition::enumerate() {
+				let column = ColumnPosition::from_chunk(index as u8, position);
 
-			blocks.set(position, block);
+				let block = if trilinear(&new_field, position) > 0.0 {
+					solid
+				} else {
+					air
+				};
+
+				blocks.set(position, block);
+			}
 		}
 	}
+}
+
+pub fn trilinear(array: &[[[f64; 5]; 5]; 3], position: ChunkPosition) -> f64 {
+	let inner = (
+		((position.y() % 8) as f64) / 8.0,
+		((position.z() % 4) as f64) / 4.0,
+		((position.x() % 4) as f64) / 4.0
+	);
+
+	let indices = (
+		(position.y() / 8) as usize,
+		(position.z() / 4) as usize,
+		(position.x() / 4) as usize
+	);
+
+	math::lerp(
+		math::lerp(
+			math::lerp(
+				array[indices.0    ][indices.1    ][indices.2    ],
+				array[indices.0    ][indices.1 + 1][indices.2    ],
+				inner.1
+			),
+			math::lerp(
+				array[indices.0 + 1][indices.1    ][indices.2    ],
+				array[indices.0 + 1][indices.1 + 1][indices.2    ],
+				inner.1
+			),
+			inner.0
+		),
+		math::lerp(
+			math::lerp(
+				array[indices.0    ][indices.1    ][indices.2 + 1],
+				array[indices.0    ][indices.1 + 1][indices.2 + 1],
+				inner.1
+			),
+			math::lerp(
+				array[indices.0 + 1][indices.1    ][indices.2 + 1],
+				array[indices.0 + 1][indices.1 + 1][indices.2 + 1],
+				inner.1
+			),
+			inner.0
+		),
+		inner.2
+	)
 }
