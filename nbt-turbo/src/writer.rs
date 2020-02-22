c685b0d05630b72e12678c85fb11a3c834c48e93
coderbot <coderbot16@gmail.com>
2020-02-22 13:37:49 -0800

    Relax bounds on CompoundWriter, add support for writing List<Compound> tags

--- nbt-turbo/src/writer.rs
+++ nbt-turbo/src/writer.rs
@@ -10,7 +10,7 @@ pub enum Kind {
 	String,
 	List,
 	Compound,
-	I32Array,
+	U32Array,
 	I64Array
 }
 
@@ -19,12 +19,23 @@ pub trait Output {
 	fn extend_from_slice(&mut self, slice: &[u8]);
 }
 
-impl<T> Output for T where T: AsMut<Vec<u8>> {
+impl<T> Output for &mut T where T: Output {
 	fn push(&mut self, value: u8) {
-		self.as_mut().push(value)
+		T::push(self, value)
 	}
+
 	fn extend_from_slice(&mut self, slice: &[u8]) {
-		self.as_mut().extend_from_slice(slice);
+		T::extend_from_slice(self, slice);
+	}
+}
+
+impl Output for Vec<u8> {
+	fn push(&mut self, value: u8) {
+		Vec::push(self, value)
+	}
+
+	fn extend_from_slice(&mut self, slice: &[u8]) {
+		Vec::extend_from_slice(self, slice)
 	}
 }
 
@@ -61,6 +72,10 @@ impl<T: Output> CompoundWriter<T> {
 		assert!(name.len() <= 32767, "Tag name too long: {}", name);
 		self.out.extend_from_slice(&u16::to_be_bytes(name.len() as u16));
 		self.out.extend_from_slice(name.as_bytes());
+	}
+
+	pub fn bool(&mut self, name: &str, value: bool) -> &mut Self {
+		self.i8(name, value as i8)
 	}
 
 	pub fn i8(&mut self, name: &str, value: i8) -> &mut Self {
@@ -105,16 +120,6 @@ impl<T: Output> CompoundWriter<T> {
 		self
 	}
 
-	pub fn u8_array(&mut self, name: &str, value: &[u8]) -> &mut Self {
-		self.header(Kind::U8Array, name);
-
-		assert!(value.len() <= std::i32::MAX as usize, "Byte array too long: {} (maximum length: {})", value.len(), std::i32::MAX);
-		self.out.extend_from_slice(&u32::to_be_bytes(value.len() as u32));
-		self.out.extend_from_slice(value);
-
-		self
-	}
-
 	pub fn string(&mut self, name: &str, value: &str) -> &mut Self {
 		self.header(Kind::String, name);
 
@@ -125,16 +130,85 @@ impl<T: Output> CompoundWriter<T> {
 		self
 	}
 
-	// TODO: List
-}
+	fn array_length(&mut self, len: usize) {
+		assert!(len <= std::i32::MAX as usize, "Array too long: {} (maximum length: {})", len, std::i32::MAX);
 
-impl<T> CompoundWriter<T> where T: Output + AsMut<Vec<u8>> {
+		self.out.extend_from_slice(&u32::to_be_bytes(len as u32));
+	}
+
+	pub fn u8_array(&mut self, name: &str, value: &[u8]) -> &mut Self {
+		self.header(Kind::U8Array, name);
+		self.array_length(value.len());
+
+		self.out.extend_from_slice(value);
+
+		self
+	}
+
+	pub fn u32_array(&mut self, name: &str, value: &[u32]) -> &mut Self {
+		self.header(Kind::U32Array, name);
+		self.array_length(value.len());
+
+		for &entry in value {
+			self.out.extend_from_slice(&entry.to_be_bytes());
+		}
+
+		self
+	}
+
 	pub fn compound_writer(&mut self, name: &str) -> CompoundWriter<&mut T> {
 		CompoundWriter::start(name, &mut self.out)
 	}
 
 	pub fn compound<F>(&mut self, name: &str, filler: F) where F: FnOnce(&mut CompoundWriter<&mut T>) {
 		let mut writer = CompoundWriter::start(name, &mut self.out);
+
+		filler(&mut writer);
+
+		writer.end();
+	}
+
+	// TODO: List
+	pub fn compound_array<F>(&mut self, name: &str, len: usize, filler: F) -> &mut Self where F: FnOnce(&mut CompoundArrayWriter<T>) {
+		self.header(Kind::List, name);
+
+		if len == 0 {
+			// Kind::End + length of 0
+			self.out.extend_from_slice(&[0; 5]);
+
+			return self;
+		}
+
+		self.out.push(Kind::Compound as u8);
+		self.array_length(len);
+
+		let mut writer = CompoundArrayWriter {
+			out: &mut self.out,
+			remaining: len
+		};
+
+		filler(&mut writer);
+
+		assert_eq!(writer.remaining, 0, "Attempted to end an incomplete CompoundArrayWriter, {} element(s) remaining", writer.remaining);
+
+		self
+	}
+}
+
+pub struct CompoundArrayWriter<'w, T: Output> {
+	out: &'w mut T,
+	remaining: usize
+}
+
+impl<'w, T> CompoundArrayWriter<'w, T> where T: Output {
+	pub fn compound<F>(&mut self, filler: F) where F: FnOnce(&mut CompoundWriter<&mut T>) {
+		assert_ne!(self.remaining, 0);
+		self.remaining -= 1;
+
+		let mut writer = CompoundWriter {
+			// re-borrow the output
+			out: &mut *self.out
+		};
 
 		filler(&mut writer);
 
