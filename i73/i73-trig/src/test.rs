d891692c9ded8eb6d48ca6f65585912d81f1dae3
coderbot <coderbot16@gmail.com>
2020-07-31 18:38:40 -0400

    Clear up remaining warnings by removing unused functions
    Remove read_f32s and read_f64s, and replace read_u64s with read_u32s

--- i73/i73-trig/src/test.rs
+++ i73/i73-trig/src/test.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
 use std::io::Read;
 
-pub fn read_u64s(name: &str) -> Vec<u64> {
+pub fn read_u32s(name: &str) -> Vec<u32> {
 	let file = File::open(format!("test_data/{}.txt", name)).unwrap();
 	let mut data = Vec::new();
 
-	let mut term = 0u64;
+	let mut term = 0u32;
 	for byte in file.bytes() {
 		let c = (byte.unwrap() as char).to_lowercase().next().unwrap();
 
@@ -16,9 +16,9 @@ pub fn read_u64s(name: &str) -> Vec<u64> {
 		} else {
 			term <<= 4;
 			term |= if c >= '0' && c <= '9' {
-				(c as u64) - ('0' as u64)
+				(c as u32) - ('0' as u32)
 			} else if c >= 'a' && c <= 'f' {
-				(c as u64) - ('a' as u64) + 10
+				(c as u32) - ('a' as u32) + 10
 			} else {
 				panic!("Bad hex character {}", c);
 			};
@@ -28,16 +28,4 @@ pub fn read_u64s(name: &str) -> Vec<u64> {
 	data.push(term);
 
 	data
-}
-
-pub fn read_u32s(name: &str) -> Vec<u32> {
-	read_u64s(name).iter().map(|&v| v as u32).collect::<Vec<_>>()
-}
-
-pub fn read_f32s(name: &str) -> Vec<f32> {
-	read_u64s(name).iter().map(|&v| f32::from_bits(v as u32)).collect::<Vec<_>>()
-}
-
-pub fn read_f64s(name: &str) -> Vec<f64> {
-	read_u64s(name).iter().map(|&v| f64::from_bits(v)).collect::<Vec<_>>()
 }
