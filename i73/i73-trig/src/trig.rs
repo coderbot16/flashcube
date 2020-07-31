043a8f468e42a2e8b2c43ecd040d02a76fba3a9f
coderbot <coderbot16@gmail.com>
2020-07-31 19:00:29 -0400

    Begin cleaning up and documenting the sin impl

--- i73/i73-trig/src/trig.rs
+++ i73/i73-trig/src/trig.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 include!(concat!(env!("OUT_DIR"), "/sin_table.rs"));
 
 pub fn sin(f: f32) -> f32 {
@@ -8,20 +10,31 @@ pub fn cos(f: f32) -> f32 {
 	sin_index(((f * 10430.38 + 16384.0) as i32) as u32)
 }
 
-fn sin_index(idx: u32) -> f32 {
-	let idx = idx & 0xFFFF;
+fn sin_index(mut index: u32) -> f32 {
+	// Clamp the input to the range of 0 ≤ x < 2π
+	index &= 0xFFFF;
 
-	let neg = (idx & 0x8000) << 16;
-	let idx2 = idx & 0x7FFF;
+	// A special case...
+	if index == 32768 {
+		// *almost* zero... it's 0.00000000000000012246469
+
+		return f32::from_bits(0x250D3132);
+	}
+
+	// Trigonometric identity: sin(-x) = -sin(x)
+	// Given a domain of 0 ≤ x < 2π, just negate the value if x > π
+	// This allows the sin table size to be halved.
+	let neg = (index & 0x8000) << 16;
+
+	// Use the half-range index
+	let idx = index & 0x7FFF;
 	let invert = (idx & 0x4000) >> 14;
 
 	let full_invert = 0u32.wrapping_sub(invert);
 	let sub_from = (invert << 15) + invert;
-	let idx3 = ::std::cmp::min(sub_from.wrapping_add(idx2 ^ full_invert), 16383);
+	let idx3 = cmp::min(sub_from.wrapping_add(idx ^ full_invert), 16383);
 
-	let wierd = (idx == 32768) as u32;
-
-	let raw = (SIN_TABLE[idx3 as usize] ^ neg).wrapping_add(wierd * 0xA50D3132);
+	let raw = SIN_TABLE[idx3 as usize] ^ neg;
 
 	f32::from_bits(raw)
 }
@@ -34,9 +47,8 @@ mod test {
 
 		assert_eq!(java.len(), 65536);
 
-		for index in 0..65536usize {
+		for (index, &j) in java.iter().enumerate() {
 			let r = super::sin_index(index as u32).to_bits();
-			let j = java[index];
 
 			if r != j {
 				panic!("trig::test_sin: mismatch @ index {}: {} (R) != {} (J)", index, r, j);
