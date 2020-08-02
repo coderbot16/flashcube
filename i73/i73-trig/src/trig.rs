ca6481dab14d31c2240677fdf072c319265d6d8f
coderbot <coderbot16@gmail.com>
2020-08-01 23:21:45 -0400

    Use a specialized function for index clamping

--- i73/i73-trig/src/trig.rs
+++ i73/i73-trig/src/trig.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-
 include!(concat!(env!("OUT_DIR"), "/sin_table.rs"));
 
 pub fn sin(f: f32) -> f32 {
@@ -30,20 +28,32 @@ fn sin_index(index: u16) -> f32 {
 }
 
 /// Computes the sin value on the range of 0 ≤ x < π
-fn sin_index_half(index: u16) -> u32 {
-	let index = index as u32;
-
+fn sin_index_half(mut index: u16) -> u32 {
 	// 1 if π/2 ≤ x, 0 otherwise
 	let invert = (index & 0x4000) >> 14;
 
-	// 0xFFFFFFFF if π/2 ≤ x, 0 otherwise
-	let full_invert = 0u32.wrapping_sub(invert);
+	// If invert is 1, then results in 0xFFFF, otherwise results in 0
+	let full_invert = 0u16.wrapping_sub(invert);
 
-	// 0xE001 if π/2 ≤ x, 0 otherwise (TODO: verify)
+	// 0x8001 if π/2 ≤ x, 0 otherwise
 	let sub_from = (invert << 15) + invert;
-	let idx3 = cmp::min(sub_from.wrapping_add(index ^ full_invert), 16383);
+	
+	// Trigonometric identity: sin(x) = sin(π/2 - x)
+	// Computes 0x8000 - index if x > π, doesn't change index otherwise.
+	index = sub_from.wrapping_add(index ^ full_invert);
 
-	SIN_TABLE[idx3 as usize]
+	// Special case: an index of 16384 has the same value as 16383
+	index = clamp_index(index);
+
+	SIN_TABLE[index as usize]
+}
+
+/// Clamps the input index to the range of 0 to 16383.
+fn clamp_index(a: u16) -> u16 {
+	let difference = a.wrapping_sub(16383);
+	let extended_sign = ((difference as i16) >> 15) as u16;
+
+	u16::wrapping_add(16383, difference & extended_sign)
 }
 
 #[cfg(test)]
